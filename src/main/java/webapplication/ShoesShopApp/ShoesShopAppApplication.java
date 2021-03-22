4a8ad7fb0a83112905e2dfed8151166202338ff5
GieDek24 <61430001+GieDek24@users.noreply.github.com>
2021-03-22 17:26:16 +0100

    init-repo

---
+++ src/main/java/webapplication/ShoesShopApp/ShoesShopAppApplication.java
@@ -0,0 +1,13 @@
+package webapplication.ShoesShopApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoesShopAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoesShopAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/webapplication/ShoesShopApp/ShoesShopAppApplicationTests.java
@@ -0,0 +1,13 @@
+package webapplication.ShoesShopApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoesShopAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
