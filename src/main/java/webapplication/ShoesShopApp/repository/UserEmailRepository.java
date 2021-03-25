e4baf4b98edf937d587afa84506dac261ee3676d
Marian <https://github.com/10marianosikJoz/Application.git>
2021-03-25 17:18:19 +0100

    Solved with email registration

---
+++ src/main/java/webapplication/ShoesShopApp/repository/UserEmailRepository.java
@@ -0,0 +1,13 @@
+package webapplication.ShoesShopApp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import webapplication.ShoesShopApp.model.User;
+import webapplication.ShoesShopApp.model.dto.UserRegistrationDto;
+
+import java.util.Optional;
+
+@Repository
+public interface UserEmailRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
